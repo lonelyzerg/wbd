a5fcce71b8b3d88a5dcf2a8477e8cb1f25f98c3c
lonelyzerg@hotmail.com <lonelyzerg@Desktop-Zerg>
2016-09-01 21:59:02 -0500

    started

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,10 +1,16 @@
+from math import floor
+import types
 class Angle():
     def __init__(self):
-        #self.angle = ...       set to 0 degrees 0 minutes
-        pass
+        self.degree = 0       #set to 0 degrees 0 minutes
+        self.minute = 0.0
     
     def setDegrees(self, degrees):
-        pass
+        inputType = types(degrees)
+        if (inputType != types.IntType) & (inputType != types.FloatType):
+            raise Exception("Angle.setDegrees: Invalid input (input is not a number)", inputType)
+        self.degree = floor(degrees)
+        self.minute = degrees - self.degree
     
     def setDegreesAndMinutes(self, degrees):
         pass
